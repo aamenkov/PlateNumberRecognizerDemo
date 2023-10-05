d114bdba659180372e570551a4c7b62d980c6a38
aamenkov <aamenkov@mail.ru>
2023-10-05 19:16:26 +0300

    Add all config and lib files

--- PlateNumberRecognizerDemo/PlateNumberRecognizerDemo.Android/MainActivity.cs
+++ PlateNumberRecognizerDemo/PlateNumberRecognizerDemo.Android/MainActivity.cs
@@ -1,21 +1,36 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using OpenALPR_Xamarin.Android_Library;
 
 namespace PlateNumberRecognizerDemo.Droid
 {
     [Activity(Label = "PlateNumberRecognizerDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public string AndroidDataDir;
+        public string OpenALPRConfigFile;
+        public string OpenALPRRuntimeFolder;
+        public string TestImagePath;
+        public OpenALPR OpenALPRInstance;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+
+            AndroidDataDir = ApplicationInfo.DataDir;
+            OpenALPRConfigFile = AndroidDataDir + "/runtime_data/openalpr.conf";
+            OpenALPRRuntimeFolder = AndroidDataDir + "/runtime_data";
+            TestImagePath = AndroidDataDir + "/runtime_data/image.jpg";
+
+            OpenALPRInstance = new OpenALPR(this, AndroidDataDir, OpenALPRConfigFile, "eu");
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
